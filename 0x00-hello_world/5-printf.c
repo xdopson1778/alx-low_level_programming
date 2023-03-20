31756bd1b6862b8765b5b32ab444295d3912de1c
xdopson1778 <xdopsonnom@gmail.com>
2023-03-20 13:16:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.>
+/**
+ * main -A program that a line using the printf function
+ * return 0 (success)
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+                return (0);
+
