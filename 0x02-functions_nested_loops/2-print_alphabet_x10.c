a00970b6253cc560d74c09e4e60dea640b57ca60
xdopson1778 <xdopsonnom@gmail.com>
2023-03-22 18:14:39 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,15 @@
 /**
  * print_alphabet_x10 - prints the alphabet in lowercase 10 times
  */
+
 void print_alphabet_x10(void)
 {
 	int i = 0;
+
 	while (i < 0)
 	{
 		char c = 'a';
+
 		while (c <= 'z')
 		{
 			_putchar(c);
