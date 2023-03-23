e35d876e706bce1406bebc9e72e07b5d5275cf5d
xdopson1778 <xdopsonnom@gmail.com>
2023-03-22 17:39:57 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - starting point
- * retrun: always return 0
+ * Return: always return 0
  */
 
 int main(void)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,3 @@
-/**
- * my main
- * return: always return 0
- */
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
